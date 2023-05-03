ce48e5d648b57c6a5a9b5814fc24518d57d21e24
Bogdan <bogdangaponenko1996@gmail.com>
2023-05-03 10:05:16 -0500

    delete never-used Link

--- src/components/MoviesByQuery/MoviesByQuery.jsx
+++ src/components/MoviesByQuery/MoviesByQuery.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { StyledLink, StyledUl } from './MoviesByQuery.styled';
 export const MoviesByQuery = ({ movies }) => {
   const location = useLocation();
